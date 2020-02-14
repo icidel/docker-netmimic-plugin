2cce14e9edd69b167720ba6a16f741addb21b0e4
del65 <icidel@users.noreply.github.com>
2020-02-14 14:00:47 +0100

    Draft for Docker plugin API implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let manifest = r#"{"Implements":["NetworkDriver"]}"#;
+    // POST /Plugin.Activate => 200 OK with body 
+    let activate = warp::path!("/Plugin.Activate").map(|| { manifest });
+	// POST /NetworkDriver.GetCapabilities -> 
+    let getCapabilities = warp::path!("/NetworkDriver.GetCapabilities").map(|| { "" });;
+	// POST /NetworkDriver.AllocateNetwork ->
+	// POST /NetworkDriver.FreeNetwork ->
+	// POST /NetworkDriver.CreateNetwork ->
+	// POST /NetworkDriver.DeleteNetwork ->
+	// POST /NetworkDriver.CreateEndpoint ->
+	// POST /NetworkDriver.EndpointOperInfo ->
+	// POST /NetworkDriver.DeleteEndpoint ->
+	// POST /NetworkDriver.Join ->
+	// POST /NetworkDriver.Leave ->
+	// POST /NetworkDriver.DiscoverNew ->
+	// POST /NetworkDriver.DiscoverDelete -> 
+	// POST /NetworkDriver.ProgramExternalConnectivity ->
+	// POST /NetworkDriver.RevokeExternalConnectivity -> 
+    let routes = warp::post().and(activate.or(getCapabilities));
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }

58ba2dfab1caca961d18d5c22f814e240762757e
del65 <icidel@users.noreply.github.com>
2020-02-18 14:14:42 +0100

    Some other tests...

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::os::unix::net::{UnixStream, UnixListener};
 use std::path::Path;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let socket = Path::new("plugin.sock");
 
     // Delete old socket if necessary
@@ -18,10 +18,22 @@ fn main() {
     println!("Server started, waiting for clients");
 
     // Iterate over clients, blocks if no client available
-    for mut stream in listener.incoming() {
-        let mut request = String::new();
-        stream.read_to_string(&mut request)?;
-        println!("Client asks: {}", request);
-        stream.write_all(b"hello world")?;
+    for streamResult in listener.incoming() {
+        match streamResult {
+            Ok(stream) => {
+                let mut request = String::new();
+                stream.read_to_string(&mut request)?;
+                println!("Client asks: {}", request);
+                stream.write_all(b"hello world")?;
+            }
+            Err(err) => {
+                // Delete socket on error
+                if socket.exists() {
+                    std::fs::remove_file(&socket);
+                }
+                break;
+            }
+        }
     }
+    Ok(()) // -> std::io::Result<()>
 }

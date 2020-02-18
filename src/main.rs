ad768b3f3602b9906e73939c0f388c48d100dd9b
del65 <icidel@users.noreply.github.com>
2020-02-18 12:17:51 +0100

    Sample socket server
    Sample socket server: to receive requests from the docker engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![feature(io)]
+#![feature(path)]
+
+use std::os::unix::net::{UnixStream, UnixListener};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let socket = Path::new("plugin.sock");
+
+    // Delete old socket if necessary
+    if socket.exists() {
+        fs::unlink(&socket).unwrap();
+    }
+
+    // Bind to socket
+    let listener = match UnixListener::bind(&socket) {
+        Err(_) => panic!("failed to bind socket"),
+        Ok(listener) => listener,
+    };
+
+    println!("Server started, waiting for clients");
+
+    // Iterate over clients, blocks if no client available
+    for mut stream in listener.incoming() {
+        println!("Client said: {}", stream.read_to_string().unwrap());
+    }
 }
